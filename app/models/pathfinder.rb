cda508ae36dddae330cef7fc7e169b8a26ceb4fb
Daniel Xutuc <danielxutuc@gmail.com>
2019-04-25 01:53:50 -0300

    relacion one to many de unidad y conquistadores

--- app/models/pathfinder.rb
+++ app/models/pathfinder.rb
@@ -1,3 +1,7 @@
 class Pathfinder < ApplicationRecord
   belongs_to :unit
+
+  def full_name
+    self.name + " " + self.last_name
+  end
 end
--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,3 +1,4 @@
 class Unit < ApplicationRecord
   belongs_to :counselor
+  has_many :pathfinders
 end
